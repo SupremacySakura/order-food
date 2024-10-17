6e2407d2e288a2a362798d6ae50e74e46d35cd44
Supremacy <3118654731@qq.com>
2024-10-17 13:44:54 +0800

    0.6.0 完成店铺导航栏与菜单的联动

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,11 +1,50 @@
 <script setup lang="ts">
-import { onBeforeMount, onMounted, ref } from 'vue'
+import { onBeforeMount, onMounted, ref, nextTick, type VNode, type VNodeRef } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import foodPhoto from '../../public/foodPhoto.png'
+//导入setting仓库
+import { useSettingStore } from '@/stores/settingStore';
+const { primary_color } = useSettingStore()
+
 const router = useRouter()
 const route = useRoute()
+//店铺id
 const id = ref()
+//最低高度
 const foodNavMinHeight = ref(0)
+const list = ref([1, 2, 3, 4, 5, 6])
+const activeIndex = ref(0) // 当前激活的导航索引
+const partList = ref(null) // 获取每个部分的ref
+const partPosition = ref<number[]>([])
+const foodList = ref(null)
+/**
+ * 处理滚动事件,与导航栏联动
+ * @param event 
+ */
+const handleScroll = (event: Event)=>{
+  const scrollTop = (event.target as HTMLElement).scrollTop
+  if(partPosition.value){
+    for(let i = 0;i<partPosition.value.length-1;i++){
+      if(scrollTop>=partPosition.value[i]-310&&scrollTop<partPosition.value[i+1]-310){
+        activeIndex.value = i
+        return
+      }
+      if(scrollTop>=partPosition.value[partPosition.value.length-1]){
+        activeIndex.value = partPosition.value.length-1
+        return
+      }
+    }
+  }
+}
+const gotoPart = (index:number)=>{
+  if(partList.value){
+    const part = partList.value[index]
+    if(part){
+      (part as HTMLElement).scrollIntoView({behavior:'smooth'})
+    }
+  }
+  
+}
 onBeforeMount(() => {
   const viewPortHeight = window.innerHeight
   foodNavMinHeight.value = viewPortHeight - 300
@@ -14,7 +53,18 @@ onMounted(() => {
   //获取店铺id
   id.value = route.query?.id
 
-
+  // 创建每个部分的 ref
+  if (partList.value) {
+    (partList.value as Array<any>).forEach(element => {
+      partPosition.value.push(element.offsetTop)
+    })
+  }
+  for(let i =0;i<partPosition.value.length;i++){
+    const emptyBlock = document.createElement('div')
+    emptyBlock.className = 'part'
+    emptyBlock.style.height = '110px'
+    foodList.value && (foodList.value as HTMLElement).appendChild(emptyBlock)
+  }
 })
 
 </script>
@@ -52,14 +102,36 @@ onMounted(() => {
 
     <div class="foodShow">
       <div class="foodNav" :style="{ minHeight: foodNavMinHeight + 'px' }">
-        <span>收藏福利</span>
-        <span>必点搭档</span>
-        <span>尝鲜新品</span>
-        <span>灵魂拌面</span>
-        <span>豪华双拼</span>
-        <span>加点浇头</span>
+        <span :class="[{ active: activeIndex === 0 }]" @click="gotoPart(0)">收藏福利</span>
+        <span :class="[{ active: activeIndex === 1 }]" @click="gotoPart(1)">必点搭档</span>
+        <span :class="[{ active: activeIndex === 2 }]" @click="gotoPart(2)">尝鲜新品</span>
+        <span :class="[{ active: activeIndex === 3 }]" @click="gotoPart(3)">灵魂拌面</span>
+        <span :class="[{ active: activeIndex === 4 }]" @click="gotoPart(4)">豪华双拼</span>
+        <span :class="[{ active: activeIndex === 5 }]" @click="gotoPart(5)">加点浇头</span>
       </div>
-      <div class="foodList"></div>
+      <div class="foodList" @scroll="handleScroll" ref="foodList">
+        <div class="part" v-for="(item, index) in list" :key="item">
+          <div class="partName" ref="partList">收藏福利</div>
+          <div class="menuItem">
+            <div>
+              <img :src="foodPhoto" alt="">
+            </div>
+            <div class="foodInfo">
+              <div>
+                <h4>辣椒蘸水</h4>
+                <span class="sales">月售2000+</span>
+                <div>
+                  <span class="label">一人份</span>
+                </div>
+              </div>
+              <div class="buy">
+                <span class="price">￥0</span>
+                <button class="addButton" :style="{ backgroundColor: primary_color }">+</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -68,7 +140,8 @@ onMounted(() => {
 <style lang="less" scoped>
 .shopBox {
   width: 100%;
-  height: 100%;
+  height: 100vh;
+  overflow: hidden;
 
   .backgroundImage {
     width: 100%;
@@ -155,6 +228,7 @@ onMounted(() => {
     position: sticky;
     top: 0;
     background-color: white;
+    z-index: 1;
 
     .searchLabel {
       font-weight: 600
@@ -167,10 +241,15 @@ onMounted(() => {
 
   .foodShow {
     width: 100%;
+    display: flex;
+    height: calc(100vh - 300px);
 
+    /* 确保这部分能够滚动, 减去搜索栏等的高度 */
     .foodNav {
       width: 20%;
       background-color: rgba(166.2, 168.6, 173.4, 0.1);
+      overflow-y: scroll;
+      border-radius: 5px;
 
       span {
         width: 100%;
@@ -179,6 +258,77 @@ onMounted(() => {
         align-items: center;
         justify-content: center;
         font-size: 12px;
+      }
+
+      .active {
+        background-color: white;
+      }
+    }
+
+    .foodList {
+      width: 80%;
+      padding: 10px;
+      overflow-y: scroll;
+
+      .part {
+        .partName {
+          height: 30px;
+          font-size: 14px;
+          background-color: white;
+          position: sticky;
+          top: -10px;
+        }
+
+        .menuItem {
+          width: 100%;
+          height: 110px;
+          display: flex;
+          padding: 10px;
+
+          img {
+            width: 90px;
+            height: 90px;
+          }
+
+          .foodInfo {
+            width: 60%;
+            margin-left: 10px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            .sales {
+              font-size: 12px;
+              color: rgb(166.2, 168.6, 173.4);
+            }
+
+            .label {
+              background-color: #CFD3DC;
+              font-size: 12px;
+              border-radius: 5px;
+              padding: 3px;
+              margin-left: 4px;
+            }
+
+            .buy {
+              display: flex;
+              justify-content: space-between;
+
+              .price {
+                font-size: 16px;
+                color: orangered;
+                font-weight: 600;
+              }
+
+              .addButton {
+                width: 25px;
+                height: 25px;
+                border-radius: 25px;
+                border: none;
+              }
+            }
+          }
+        }
       }
     }
   }
